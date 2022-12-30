45505bd335214e80cbf5d8e6a475357efbf2ded3
meshubhampol <maheshpol9@gmail.com>
2022-12-30 23:51:04 +0530

    minor change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function showAlert() {
     let fullName=document.getElementById("full-name").value;
     let lastName=document.getElementById("last-name").value;
     let email=document.getElementById("email-address").value;
-
-    if(fullName.length>0 && lastName.length>0 && email.length>0 && email.includes("@")) {
+    let reason=document.getElementById("reason-input").value;
+    if(fullName.length>0 && lastName.length>0 && email.length>0 && email.includes("@") && reason.length>0) {
         alert("Success");
     }
 }
