46f68e993e9803db10c81b563182959c16bbb4f0
meshubhampol <maheshpol9@gmail.com>
2022-12-30 22:37:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    let fullName=document.getElementById("full-name").value;
+    let lastName=document.getElementById("last-name").value;
+    let email=document.getElementById("email-address").value;
+
+    if(fullName.length>0 && lastName.length>0 && email.length>0 && email.includes("@")) {
+        alert("Success");
+    }
+}
